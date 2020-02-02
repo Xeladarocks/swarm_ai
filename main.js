511279c43e19fa49c730af15fca860efceea0481
Xeladarocks <xeladarocks@gmail.com>
2020-02-01 21:57:23 -0500

    final

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function setup() {
     createCanvas(800, 800);
-    frameRate(20);
+    frameRate(60);
+    textSize(15);
 
     alignSlider = createSlider(0, 2, 1.4, 0.1);
+    alignSlider.position((windowWidth-width)/2+20, (windowHeight-height)/2+80);
     cohesionSlider = createSlider(0, 2, 0.8, 0.1);
+    cohesionSlider.position((windowWidth-width)/2+20, (windowHeight-height)/2+50);
     separationSlider = createSlider(0, 2, 1.6, 0.1);
+    separationSlider.position((windowWidth-width)/2+20, (windowHeight-height)/2+20);
 
     swarm = new Swarm();
     swarm.create_pool(64);
@@ -12,6 +16,11 @@ function setup() {
 
 function draw() {
     background(250);
+    fill(0);
+    noStroke();
+    text("separation", 30+separationSlider.width, separationSlider.height+15);
+    text("cohesion", 30+cohesionSlider.width, cohesionSlider.height+45);
+    text("align", 30+alignSlider.width, alignSlider.height+75);
 
     for(let i=0; i < swarm.boids.length; i++) {
         let boid = swarm.boids[i];
